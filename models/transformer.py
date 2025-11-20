2329838557747642525979b4d04033e846f440db
Lixing Wang <sabrina_xm@me.com>
2025-11-19 22:16:38 -0500

    code:model done

--- models/transformer.py
+++ models/transformer.py
@@ -8,13 +8,13 @@ class ViTConfig:
     num_channels: int
     patch_height: int
     patch_width: int
+    patch_overlap: int
     num_layers: int
     hidden_dim: int
     mlp_dim: int
     num_heads: int
     dropout_rate: float
     attention_dropout_rate: float
-    max_token_length: int # required for relative time embeddings
     
 # Tiny ViT with 16x16 patches, ~5M parameters
 ViT-Ti-16 = ViTConfig(
@@ -23,6 +23,7 @@ ViT-Ti-16 = ViTConfig(
     num_channels=3,
     patch_height=16,
     patch_width=16,
+    patch_overlap=0,
     num_layers=12,
     hidden_dim=192,
     mlp_dim=768,
@@ -39,6 +40,7 @@ ViT-S-16 = ViTConfig(
     num_channels=3,
     patch_height=16,
     patch_width=16,
+    patch_overlap=0,
     num_layers=12,
     hidden_dim=384,
     mlp_dim=1536,
@@ -55,6 +57,7 @@ ViT-B-16 = ViTConfig(
     num_channels=3,
     patch_height=16,
     patch_width=16,
+    patch_overlap=0,
     num_layers=12,
     hidden_dim=768,
     mlp_dim=3072,
@@ -81,6 +84,15 @@ class ViT(tf.keras.layers.Layer):
     
     def call(self, x, training):
         # x shape: (batch_size, num_patches, image_height, image_width, num_channels)
+        # Ordering of patches matter!
+        # This is a spectrogram image:
+        # [ [patch_1, patch_2, ..., patch_n],
+        #   [patch_(n+1), patch_(n+2), ..., patch_(2n)],
+        #   ...
+        #   [patch_((m-1)*n+1), ..., patch_(m*n)] ]
+        # --> time goes along rows, frequency along columns
+        # where each patch is of shape (patch_height, patch_width, num_channels)
+        
         x = tf.reshape(x, [tf.shape(x)[0], tf.shape(x)[1], -1])  # flatten patches
         x = self.linear_projection(x)  # project to hidden_dim
         # prepend cls token to the sequence
@@ -96,17 +108,11 @@ class TransformerBlock(tf.keras.layers.Layer):
     def __init__(self, config: ViTConfig, **kwargs):
         super(TransformerBlock, self).__init__(**kwargs)
         self.self_attention = SelfAttention(
-            hidden_dim=config.hidden_dim,
-            num_heads=config.num_heads,
-            dropout_rate=config.dropout_rate,
-            attention_dropout_rate=config.attention_dropout_rate,
-            max_token_length=config.max_token_length,
+            config,
             initializer=tf.keras.initializers.HeUniform()
         )
         self.mlp_block = MLPBlock(
-            hidden_dim=config.hidden_dim,
-            mlp_dim=config.mlp_dim,
-            dropout_rate=config.dropout_rate,
+            config,
             initializer=tf.keras.initializers.HeUniform()
         )
     
@@ -118,10 +124,10 @@ class TransformerBlock(tf.keras.layers.Layer):
         return x
 
 class SelfAttention(tf.keras.layers.Layer):
-    def __init__(self, hidden_dim, num_heads, dropout_rate, attention_dropout_rate, max_token_length, initializer, **kwargs):
+    def __init__(self, config: ViTConfig, initializer, **kwargs):
         super(SelfAttention, self).__init__(**kwargs)
-        self.mha = MultiHeadAttention(num_heads=num_heads, key_dim=hidden_dim // num_heads, attention_dropout_rate=attention_dropout_rate, max_token_length=max_token_length, initializer=initializer)
-        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+        self.mha = MultiHeadAttention(config, initializer=initializer)
+        self.dropout = tf.keras.layers.Dropout(config.dropout_rate)
         self.layernorm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
     def call(self, x, training):
@@ -131,11 +137,11 @@ class SelfAttention(tf.keras.layers.Layer):
         return out
 
 class MLPBlock(tf.keras.layers.Layer):
-    def __init__(self, hidden_dim, mlp_dim, dropout_rate, initializer, **kwargs):
+    def __init__(self, config: ViTConfig, initializer, **kwargs):
         super(MLPBlock, self).__init__(**kwargs)
-        self.dense1 = tf.keras.layers.Dense(mlp_dim, activation='gelu', kernel_initializer=initializer)
-        self.dense2 = tf.keras.layers.Dense(hidden_dim, kernel_initializer=initializer)
-        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+        self.dense1 = tf.keras.layers.Dense(config.mlp_dim, activation='gelu', kernel_initializer=initializer)
+        self.dense2 = tf.keras.layers.Dense(config.hidden_dim, kernel_initializer=initializer)
+        self.dropout = tf.keras.layers.Dropout(config.dropout_rate)
         self.layernorm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
     def call(self, x, training):
@@ -147,12 +153,12 @@ class MLPBlock(tf.keras.layers.Layer):
         return out
 
 class MultiHeadAttention(tf.keras.layers.Layer):
-    def __init__(self, num_heads, key_dim, attention_dropout_rate, max_token_length, initializer, **kwargs):
+    def __init__(self, config: ViTConfig, initializer, **kwargs):
         super(MultiHeadAttention, self).__init__(**kwargs)
-        self.heads = [SingleHeadAttention(key_dim, attention_dropout_rate, max_token_length, initializer=initializer, name=f"head_{i}") for i in range(num_heads)]
-        self.num_heads = num_heads
-        self.key_dim = key_dim # key_dim is for per head, so hidden_dim = num_heads * key_dim
-        self.output_dense = tf.keras.layers.Dense(num_heads * key_dim, kernel_initializer=initializer)
+        self.key_dim = config.hidden_dim // config.num_heads # key_dim is for per head, so hidden_dim = num_heads * key_dim
+        self.heads = [SingleHeadAttention(self.key_dim, config, initializer=initializer, name=f"head_{i}") for i in range(config.num_heads)]
+        self.num_heads = config.num_heads
+        self.output_dense = tf.keras.layers.Dense(config.hidden_dim, kernel_initializer=initializer)
     
     def call(self, embeddings, training):
         head_outputs = [head(embeddings, training=training) for head in self.heads]
@@ -162,21 +168,31 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         return output
 
 class SingleHeadAttention(tf.keras.layers.Layer):
-    def __init__(self, key_dim, attention_dropout_rate, max_token_length, initializer, **kwargs):
+    def __init__(self, key_dim, config: ViTConfig, initializer, **kwargs):
         super(SingleHeadAttention, self).__init__(**kwargs)
         self.key_dim = key_dim
         self.query_dense = tf.keras.layers.Dense(key_dim, kernel_initializer=initializer)
         self.key_dense = tf.keras.layers.Dense(key_dim, kernel_initializer=initializer)
         self.value_dense = tf.keras.layers.Dense(key_dim, kernel_initializer=initializer)
-        self.attention_dropout = tf.keras.layers.Dropout(attention_dropout_rate)
-        self.max_token_length = max_token_length
+        self.attention_dropout = tf.keras.layers.Dropout(config.attention_dropout_rate)
+        
+        self.num_patch_per_col = (config.image_height - config.patch_overlap) // (config.patch_height - config.patch_overlap)
+        self.num_patch_per_row = (config.image_width - config.patch_overlap) // (config.patch_width - config.patch_overlap)
+
+        self.max_time_distance = 2 * self.num_patch_per_col - 1 # relative position bias for time dimension
         self.attention_bias_table = self.add_weight(
-            shape=(2 * max_token_length - 1,),
+            shape=(2 * config.max_time_distance - 1,),
             initializer=tf.keras.initializers.Zeros(),
             trainable=True,
-            name="attention_bias_table",
+            name="time_attention_bias_table",
         )
-    
+        self.cls_token_bias_table = self.add_weight(
+            shape=(self.num_patch_per_col * self.num_patch_per_row + 1,),
+            initializer=tf.keras.initializers.Zeros(),
+            trainable=True,
+            name="cls_token_bias_table",
+        )
+        
     def call(self, embeddings, training):
         Q = self.query_dense(embeddings) # (batch_size, seq_len, key_dim)
         K = self.key_dense(embeddings)   # (batch_size, seq_len, key_dim)
@@ -190,13 +206,25 @@ class SingleHeadAttention(tf.keras.layers.Layer):
         attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1) # (batch_size, seq_len, seq_len)
         # Add relative position bias
         # class embedding receives bias normally as other embeddings
-        seq_len = tf.shape(embeddings)[1]
-        position_indices = tf.range(seq_len)
-        relative_positions = position_indices[None, :] - position_indices[:, None] # (seq_len, seq_len) generate position difference matrix
-        clipped_relative_positions = tf.clip_by_value(relative_positions, -self.max_token_length + 1, self.max_token_length - 1) # prevent out-of-bounds
-        bias_indices = clipped_relative_positions + self.max_token_length - 1 # shift to non-negative
-        relative_position_bias = tf.gather(self.attention_bias_table, bias_indices) # (seq_len, seq_len)
-        attention_weights += relative_position_bias
+        # Assuming seq_len is always fixed during training and inference
+        # So we use max_time_distance to build the relative position bias table
+        
+        position_indices = tf.range(self.num_patch_per_row)
+        relative_positions = position_indices[None, :] - position_indices[:, None] # generate position difference matrix
+        clipped_relative_positions = tf.clip_by_value(relative_positions, -self.max_time_distance + 1, self.max_time_distance - 1) # prevent out-of-bounds
+        bias_indices = clipped_relative_positions + self.max_time_distance - 1 # shift to non-negative
+        relative_time_bias = tf.gather(self.attention_bias_table, bias_indices) # (num_patch_per_row, num_patch_per_row)
+        # Expand by repeating in both dimensions to match seq_len
+        relative_position_bias = tf.repeat(relative_time_bias, repeats=self.num_patch_per_col, axis=0)
+        relative_position_bias = tf.repeat(relative_position_bias, repeats=self.num_patch_per_col, axis=1)
+        # Now shape is (seq_len - 1, seq_len - 1), need to add in class token bias table
+        # Add class token bias to first row and first column
+        relative_time_bias = tf.concat(
+            [self.cls_token_bias_table[:,None], tf.concat([self.cls_token_bias_table[1:], relative_position_bias], axis=0)],
+            axis=1
+        ) # (seq_len, seq_len)
+        
+        attention_weights += relative_time_bias
         
         attention_weights = self.attention_dropout(attention_weights, training=training)
 
